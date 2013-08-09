094cf95b9ace13fe911d5e7268c5c409c86206ee
Guilherme Oliveira <ghophp@gmail.com>
2013-08-09 16:35:18 -0300

    remove the test to robots, that not work well, and add a no-cache way to prevent 304 code on load img, that results on error

--- jquery.ytaccess.js
+++ jquery.ytaccess.js
@@ -3,10 +3,8 @@ $.hasyt = function() {
 	if(typeof document['hasyt'] === 'undefined'){
 
 		document['hasyt'] = false;
-		$("<img/>").attr("src", "//youtube.com/favicon.ico").load(function() {
-			$("<img/>").attr("src", "//youtube.com/robots.txt").load(function() {
-				document['hasyt'] = true;
-			});
+		$("<img/>").attr("src", "//youtube.com/favicon.ico?" + new Date().getTime()).load(function() {
+			document['hasyt'] = true;
 		});
 	}
 
