a4f935146895c5a1ee81018502ffa20b0e75d38d
Guilherme Oliveira <ghophp@gmail.com>
2013-08-09 16:43:09 -0300

    add callback capability, to avoid unload state

--- jquery.ytaccess.js
+++ jquery.ytaccess.js
@@ -1,10 +1,19 @@
-$.hasyt = function() {
+$.hasyt = function(callback) {
 
 	if(typeof document['hasyt'] === 'undefined'){
 
 		document['hasyt'] = false;
 		$("<img/>").attr("src", "//youtube.com/favicon.ico?" + new Date().getTime()).load(function() {
+			
 			document['hasyt'] = true;
+			if(typeof callback !== 'undefined') {
+				callback();
+			}
+			
+		}).error(function(){
+			if(typeof callback !== 'undefined') {
+				callback();
+			}
 		});
 	}
 
